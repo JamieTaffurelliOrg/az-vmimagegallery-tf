0458be0727b67f0a3066f6a403ade60e1e899d9d
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-12-05 11:01:27 +0000

    'upped versions'

--- main.tf
+++ main.tf
@@ -53,13 +53,13 @@ resource "azurerm_storage_account" "storage" {
   location                        = var.location
   resource_group_name             = var.resource_group_name
   account_kind                    = "StorageV2"
-  account_tier                    = "Standard"
+  account_tier                    = "Premium"
   account_replication_type        = "GRS"
   access_tier                     = "Hot"
   enable_https_traffic_only       = true
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = true
+  shared_access_key_enabled       = false
   default_to_oauth_authentication = true
 
   blob_properties {
